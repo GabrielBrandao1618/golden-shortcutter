4c363e5b10eabdad4768cc38c732050c953fc086
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-06-09 15:47:10 -0300

    Adicionado código para resolver problemas de deploy heroku

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 
@@ -16,5 +17,11 @@ func main() {
 	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Access-Control-Allow-Origin"})
 	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET"})
 
-	http.ListenAndServe(":8080", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(routes.GetRouter()))
+	port, err := os.Getenv("PORT")
+
+	if err != nil {
+		port = "3000"
+	}
+
+	http.ListenAndServe(":"+port, handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(routes.GetRouter()))
 }
