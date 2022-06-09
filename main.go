c0ff4af7c85a94ac4ab317b39131b44c0b12a8b7
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-06-09 15:34:14 -0300

    Removido código desnecessário

--- main.go
+++ main.go
@@ -5,21 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/handlers"
-	"github.com/joho/godotenv"
 
 	"github.com/GabrielBrandao1618/golden-shortcutter/database"
 	"github.com/GabrielBrandao1618/golden-shortcutter/routes"
 )
 
-func setupEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
-
 func main() {
-	setupEnv()
 	database.Migrate()
 
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
